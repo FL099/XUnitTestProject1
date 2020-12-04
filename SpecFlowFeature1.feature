0e998282bdb3e6e53677fe2c65f4196d3056c091
Florian <florian@sixtl.at>
2020-12-04 14:13:09 +0100

    Alle Tests positiv

--- SpecFlowFeature1.feature
+++ SpecFlowFeature1.feature
@@ -9,3 +9,24 @@ Scenario: Add two numbers
 	And the second number is 70
 	When the two numbers are added
 	Then the result should be 120
+
+@mytag
+Scenario: Subtract two numbers
+	Given the first number is 70
+	And the second number is 50
+	When the two numbers are subtracted
+	Then the result should be 20
+
+@mytag
+Scenario: Multiply two numbers
+	Given the first number is 50
+	And the second number is 3
+	When the two numbers are multiplied
+	Then the result should be 150
+
+@mytag
+Scenario: Divide two numbers
+	Given the first number is 50
+	And the second number is 5
+	When the first number is divided by the second
+	Then the result should be 10
---
+++ SpecFlowFeature1Steps.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private int num1;
+        private int num2;
+        private float outp;
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            num1 = p0;
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            num2 = p0;
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            outp = num1 + num2;
+        }
+        
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            outp = num1 - num2;
+        }
+        
+        [When(@"the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            outp = num1 * num2;
+        }
+        
+        [When(@"the first number is divided by the second")]
+        public void WhenTheFirstNumberIsDividedByTheSecond()
+        {
+            outp = num1 / num2;
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            float expected = p0;
+            float actual = outp;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
